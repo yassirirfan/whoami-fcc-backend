a655fb720ef3a4756e3aca301744856b80b0895d
Yassir Irfan <yassirirfan7@gmail.com>
2021-06-05 20:55:12 +0530

    initial

--- server.js
+++ server.js
@@ -25,6 +25,13 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+app.get("/api/whoami", (req,res) => { 
+  res.json({
+    ipaddress: req.connection.remoteAddress,
+    language: "JavaScript",
+    software:"timestamp"
+  })
+})
 
 
 // listen for requests :)
